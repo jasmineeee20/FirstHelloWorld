e259c4dfb8843ae3d65a353cf4d7fc64111910e0
g15913ih <g15913ih@s30418.ACTSUDA>
2017-04-14 12:00:46 +0900

    日本語訳を表示

--- src/firsthelloworld/FirstHelloWorld.java
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -16,6 +16,7 @@ public class FirstHelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("こんにちは、世界！！！");
         // TODO code application logic here
     }
     
